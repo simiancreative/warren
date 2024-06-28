82b5f12f7c149df738e4d9e8b574c81036105171
Ross Nelson <axcess1@me.com>
2024-06-28 14:44:58 -0400

    Add Providers Package

--- packages/providers/jest.config.ts
+++ packages/providers/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'warren',
+  displayName: 'providers',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/warren',
+  coverageDirectory: '../../coverage/packages/providers',
 };
