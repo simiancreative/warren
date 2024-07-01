474943d75b8634e9813527b3f71100c4c4bb4907
Ross Nelson <axcess1@me.com>
2024-06-30 21:52:58 -0400

    update push to main workflow

--- bin/prep-publish.sh
+++ bin/prep-publish.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
-
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-SOURCE_PATH=${SCRIPT_DIR}/../package.json
+SOURCE_PATH=${SCRIPT_DIR}/../version.txt
 
 # get the current version from package.json
-NEW_VERSION=$(cat ${SOURCE_PATH} | jq -r '.version')
+NEW_VERSION=$(cat ${SOURCE_PATH})
 
 # update the version in package.json
 jq ".version = \"$NEW_VERSION\"" package.json >tmp.$$.json \
   && mv tmp.$$.json package.json
 
-
+echo '//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}' > .npmrc
+echo 'registry=https://registry.npmjs.org/' >> .npmrc
+echo 'always-auth=true' >> .npmrc
